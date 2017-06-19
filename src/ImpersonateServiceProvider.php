147e841941f3940accf085168fed8994daa786c8
aocneanu <aocneanu@gmail.com>
2017-06-19 17:03:09 +0000

    Apply fixes from StyleCI

--- src/ImpersonateServiceProvider.php
+++ src/ImpersonateServiceProvider.php
@@ -10,7 +10,7 @@ class ImpersonateServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app['router']->aliasMiddleware('impersonate', Impersonate::class);
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'laravel-enso/impersonate');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/impersonate');
     }
 
     public function register()

beffc71711fc87b3fa98ddb381220f0ede30d0e6
Adrian Ocneanu <aocneanu@gmail.com>
2017-07-12 12:41:04 +0000

    Apply fixes from StyleCI

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,9 +5,9 @@ Route::middleware(['web', 'auth', 'core'])
     ->group(function () {
         Route::prefix('core/impersonate')->as('core.impersonate.')
             ->group(function () {
-            	Route::get('stop', 'ImpersonateController@stop')
-                	->name('stop');
+                Route::get('stop', 'ImpersonateController@stop')
+                    ->name('stop');
                 Route::get('/{user}', 'ImpersonateController@start')
-                	->name('start');
+                    ->name('start');
             });
     });

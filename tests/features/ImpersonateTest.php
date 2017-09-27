7de0d28c342f5a361f9bf03859dd24e1d3e2ba01
mishu <mishu@xtelecom.ro>
2017-09-27 15:19:44 +0300

    fix tests

--- tests/features/ImpersonateTest.php
+++ tests/features/ImpersonateTest.php
@@ -3,7 +3,6 @@
 use App\Owner;
 use App\User;
 use Faker\Factory;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use LaravelEnso\MenuManager\app\Models\Menu;
 use LaravelEnso\PermissionManager\app\Models\Permission;
@@ -22,7 +21,7 @@ class ImpersonateTest extends TestCase
     {
         parent::setUp();
 
-        $this->disableExceptionHandling();
+        // $this->withoutExceptionHandling();
         $this->faker = Factory::create();
     }
 
@@ -42,9 +41,8 @@ class ImpersonateTest extends TestCase
     {
         $this->setUpUsers($this->defaultAccessRole());
 
-        $this->expectException(EnsoException::class);
-
-        $this->get(route('core.impersonate.start', $this->userToImpersonate->id, false));
+        $response = $this->get(route('core.impersonate.start', $this->userToImpersonate->id, false))
+            ->assertStatus(403);
     }
 
     /** @test */
@@ -52,10 +50,9 @@ class ImpersonateTest extends TestCase
     {
         $this->setUpUsers($this->adminRole());
 
-        $this->expectException(AuthorizationException::class);
-
-        $this->withSession(['impersonating' => $this->userToImpersonate->id])
-            ->get('/core/impersonate/'.$this->userToImpersonate->id);
+        $response = $this->withSession(['impersonating' => $this->userToImpersonate->id])
+            ->get('/core/impersonate/' . $this->userToImpersonate->id)
+            ->assertStatus(403);
     }
 
     /** @test */
@@ -64,9 +61,8 @@ class ImpersonateTest extends TestCase
         $this->userToImpersonate = $this->createUser('userToImpersonate', $this->adminRole());
         $this->actingAs($this->userToImpersonate);
 
-        $this->expectException(AuthorizationException::class);
-
-        $this->get('/core/impersonate/'.$this->userToImpersonate->id);
+        $this->get('/core/impersonate/' . $this->userToImpersonate->id)
+            ->assertStatus(403);
     }
 
     /** @test */
@@ -83,7 +79,7 @@ class ImpersonateTest extends TestCase
 
     private function setUpUsers(Role $role)
     {
-        $this->impersonator = $this->createUser('impersonator', $role);
+        $this->impersonator      = $this->createUser('impersonator', $role);
         $this->userToImpersonate = $this->createUser('userToImpersonate', $role);
 
         $this->actingAs($this->impersonator);
@@ -94,10 +90,10 @@ class ImpersonateTest extends TestCase
         $menu = Menu::first(['id']);
 
         $role = Role::create([
-            'name'                 => 'adminRole',
-            'display_name'         => $this->faker->word,
-            'description'          => $this->faker->sentence,
-            'menu_id'              => $menu->id,
+            'name'         => 'adminRole',
+            'display_name' => $this->faker->word,
+            'description'  => $this->faker->sentence,
+            'menu_id'      => $menu->id,
         ]);
 
         $permissions = Permission::pluck('id');
@@ -111,10 +107,10 @@ class ImpersonateTest extends TestCase
         $menu = Menu::first(['id']);
 
         $role = Role::create([
-            'name'                 => 'defaultAccessRole',
-            'display_name'         => $this->faker->word,
-            'description'          => $this->faker->sentence,
-            'menu_id'              => $menu->id,
+            'name'         => 'defaultAccessRole',
+            'display_name' => $this->faker->word,
+            'description'  => $this->faker->sentence,
+            'menu_id'      => $menu->id,
         ]);
 
         $permissions = Permission::implicit()->pluck('id');
@@ -126,15 +122,15 @@ class ImpersonateTest extends TestCase
     private function createUser($firstName, $role)
     {
         $user = new User([
-            'first_name'                 => $firstName,
-            'last_name'                  => $this->faker->lastName,
-            'phone'                      => $this->faker->phoneNumber,
-            'is_active'                  => 1,
+            'first_name' => $firstName,
+            'last_name'  => $this->faker->lastName,
+            'phone'      => $this->faker->phoneNumber,
+            'is_active'  => 1,
         ]);
-        $user->email = $this->faker->email;
-        $owner = Owner::first(['id']);
+        $user->email    = $this->faker->email;
+        $owner          = Owner::first(['id']);
         $user->owner_id = $owner->id;
-        $user->role_id = $role->id;
+        $user->role_id  = $role->id;
         $user->save();
 
         return $user;

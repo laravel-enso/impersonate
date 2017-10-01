3cef825b028bef8e227477fadd71e2290b06eb06
mishu <mishu@xtelecom.ro>
2017-10-01 22:52:20 +0300

    wip before changing the impersonate mechanism

--- src/ImpersonateServiceProvider.php
+++ src/ImpersonateServiceProvider.php
@@ -11,7 +11,7 @@ class ImpersonateServiceProvider extends ServiceProvider
     {
         $this->app['router']->aliasMiddleware('impersonate', Impersonate::class);
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
     }
 
     public function register()

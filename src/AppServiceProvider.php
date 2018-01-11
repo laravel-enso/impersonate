02b5b8a8740776c524fab9528f7dd75926ebc5b9
mishu <mishu@xtelecom.ro>
2018-01-11 10:58:19 +0200

    renames app service provider

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace LaravelEnso\Impersonate;
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\Impersonate\app\Http\Middleware\Impersonate;
 
-class ImpersonateServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {

909d040bce27546d1e5ab1afedd8dbe65b322beb
mishu <mishu@xtelecom.ro>
2018-05-28 13:24:50 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
+++ src/database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
@@ -9,7 +9,7 @@ class CreateStructureForImpersonate extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.impersonate.start', 'description' => 'Start impersonating user', 'type' => 0, 'default' => false],
-        ['name' => 'core.impersonate.stop', 'description' => 'Stop impersonating user', 'type' => 0, 'default' => true],
+        ['name' => 'core.impersonate.start', 'description' => 'Start impersonating user', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.impersonate.stop', 'description' => 'Stop impersonating user', 'type' => 0, 'is_default' => true],
     ];
 }

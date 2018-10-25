042c4d83b6f76d7d8ef1f5280a7a1ba3f2141367
mishu <mishu@xtelecom.ro>
2018-10-25 10:05:59 +0300

    updates composer.json

--- src/database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
+++ src/database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
@@ -4,10 +4,6 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 
 class CreateStructureForImpersonate extends StructureMigration
 {
-    protected $permissionGroup = [
-        'name' => 'core.impersonate', 'description' => 'Impersonate permissions group',
-    ];
-
     protected $permissions = [
         ['name' => 'core.impersonate.start', 'description' => 'Start impersonating user', 'type' => 0, 'is_default' => false],
         ['name' => 'core.impersonate.stop', 'description' => 'Stop impersonating user', 'type' => 0, 'is_default' => true],

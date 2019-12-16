891a0187b4a6af35740a00105f91474cb37e7891
Mihai <mihai.ocneanu@gmail.com>
2019-12-16 12:23:22 +0200

    reverted routes split

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,5 +4,6 @@ Route::middleware(['web', 'auth', 'core'])
     ->namespace('LaravelEnso\Impersonate\app\Http\Controllers')
     ->prefix('api/core/impersonate')->as('core.impersonate.')
     ->group(function () {
-        require 'app/impersonate.php';
+        Route::get('stop', 'Stop')->name('stop');
+        Route::get('{user}', 'Start')->name('start');
     });
--- src/routes/app/impersonate.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Route::get('stop', 'Stop')->name('stop');
-Route::get('{user}', 'Start')->name('start');

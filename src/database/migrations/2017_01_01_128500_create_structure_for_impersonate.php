0decbe7283c1a6df4454e8a019757e1726f850c9
mishu <mishu@xtelecom.ro>
2020-03-14 18:30:44 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
+++ src/database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
@@ -1,12 +1,11 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForImpersonate extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.impersonate.start', 'description' => 'Start impersonating user', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'core.impersonate.stop', 'description' => 'Stop impersonating user', 'type' => Types::Read, 'is_default' => true],
+        ['name' => 'core.impersonate.start', 'description' => 'Start impersonating user', 'is_default' => false],
+        ['name' => 'core.impersonate.stop', 'description' => 'Stop impersonating user', 'is_default' => true],
     ];
 }

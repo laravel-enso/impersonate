a48089ee2ab268bfb9683e68d4055a3e6287d8a4
reza <raftx24@gmail.com>
2020-06-05 22:10:47 +0430

    check session for stateless requests

--- src/App/Http/Middleware/Impersonate.php
+++ src/App/Http/Middleware/Impersonate.php
@@ -3,14 +3,16 @@
 namespace LaravelEnso\Impersonate\App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 class Impersonate
 {
     public function handle($request, Closure $next)
     {
-        if ($request->session()->has('impersonating')) {
-            Auth::onceUsingId(
+        if ($request->hasSession() && $request->session()->has('impersonating')) {
+            $this->guard()->onceUsingId(
                 $request->session()->get('impersonating')
             );
 
@@ -18,5 +20,10 @@ class Impersonate
         }
 
         return $next($request);
+    }
+
+    private function guard(): StatefulGuard
+    {
+        return Auth::guard(Config::get('sanctum.guard', 'web'));
     }
 }

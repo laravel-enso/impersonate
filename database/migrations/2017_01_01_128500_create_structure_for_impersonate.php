f50e5f6c6a86a73a38086967a0de1330a8c56f34
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 14:13:45 +0200

    updated migration;

--- database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
+++ database/migrations/2017_01_01_128500_create_structure_for_impersonate.php
@@ -2,10 +2,10 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForImpersonate extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'core.impersonate.start', 'description' => 'Start impersonating user', 'is_default' => false],
         ['name' => 'core.impersonate.stop', 'description' => 'Stop impersonating user', 'is_default' => true],
     ];
-}
+};
